309e7a0901a2fd607ad6f5ab0488eeb455cb9085
Aliaksandr <sigai.aleksandr@mail.ru>
2023-12-17 13:57:15 +0200

    feat(users.models): add user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class User(AbstractUser):
+    """User's model"""
+
+    number_validator = RegexValidator(regex=[r'^\+\d{9,15}$'],
+                                      message='Phone number should start from "+" and consists of 9-15 digits')
+
+    username = None
+    phone = models.CharField(max_length=15, verbose_name='phone', unique=True, validators=[number_validator])
+    email = models.EmailField(unique=True, verbose_name='email', **NULLABLE)
+    city = models.CharField(max_length=135, verbose_name='city', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='avatar', **NULLABLE)
+
+    USERNAME_FIELD = "phone"
+    REQUIRED_FIELDS = []
+
