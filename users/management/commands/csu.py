ecfa2cf3f61a1bcc4d092e7cecd23c185a272966
Aliaksandr <sigai.aleksandr@mail.ru>
2023-12-17 16:06:26 +0200

    refactor(users): add superuser
    add command to create superuser

---
+++ users/management/commands/csu.py
@@ -0,0 +1,18 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    """Commad to create superuser"""
+
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            phone='+1234567890',
+            first_name='Admin',
+            last_name='Admin',
+            is_staff=True,
+            is_superuser=True,
+        )
+        user.set_password('123qwe456asd')
+        user.save()
